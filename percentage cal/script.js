dc542a0de98219a22c691be7120f21e2926e9e52
AliHamza116 <142170084+AliHamza116@users.noreply.github.com>
2024-10-20 11:17:59 -0700

    Add files via upload

---
+++ percentage cal/script.js
@@ -0,0 +1,14 @@
+let marks= prompt("enter the no");
+
+
+if(marks<=100 && marks>=80){
+    console.log("A");
+}else if(marks<=79 && marks>=70){
+    console.log("B");
+}else if(marks<=69 && marks>=60){
+    console.log("C");
+}else if(marks<=59 && marks>=50){
+    console.log("D");
+}else {
+    console.log("F");
+}
